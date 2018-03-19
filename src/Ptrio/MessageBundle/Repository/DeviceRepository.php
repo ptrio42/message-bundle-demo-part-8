11eec4de3f6f2cfab27c8dbcee62520d0f213921
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-19 22:54:20 +0100

    Create DeviceRepository interface.

---
+++ src/Ptrio/MessageBundle/Repository/DeviceRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.czarnoleski
+ * Date: 19/03/2018
+ * Time: 22:52
+ */
+
+namespace App\Ptrio\MessageBundle\Repository;
+
+
+class DeviceRepository
+{
+
+}
