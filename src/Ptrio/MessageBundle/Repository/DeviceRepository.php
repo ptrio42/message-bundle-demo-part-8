c10afc64e521f74de4bd9b17caf3296af8ade345
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-19 22:54:49 +0100

    Add DeviceRepository class.

--- src/Ptrio/MessageBundle/Repository/DeviceRepository.php
+++ src/Ptrio/MessageBundle/Repository/DeviceRepository.php
@@ -1,15 +1,31 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 19/03/2018
- * Time: 22:52
- */
 
 namespace App\Ptrio\MessageBundle\Repository;
-
-
-class DeviceRepository
+use Doctrine\ORM\{
+    EntityManagerInterface, EntityRepository
+};
+class DeviceRepository extends
+    EntityRepository implements
+    DeviceRepositoryInterface
 {
-
+    public function __construct(EntityManagerInterface $em, string $class)
+    {
+        parent::__construct($em, $em->getClassMetadata($class));
+    }
+    
+    public function findDevicesByNames(array $deviceNames): array
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+        $qb
+            ->select('d')
+            ->from($this->getEntityName(), 'd')
+        ;
+        for ($i = 0; $i < count($deviceNames); $i++) {
+            $qb
+                ->orWhere($qb->expr()->eq('d.name', ':param_'.$i))
+                ->setParameter(':param_'.$i, $deviceNames[$i])
+            ;
+        }
+        return $qb->getQuery()->getResult();
+    }
 }
