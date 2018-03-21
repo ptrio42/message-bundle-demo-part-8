978b16f6d783a5d335ae29f83af2eee879fd9af6
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-21 12:03:54 +0100

    Create user entity class.

--- src/Ptrio/MessageBundle/Entity/User.php
+++ src/Ptrio/MessageBundle/Entity/User.php
@@ -1,15 +1,35 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 21/03/2018
- * Time: 11:55
- */
 
 namespace App\Ptrio\MessageBundle\Entity;
 
+use App\Ptrio\MessageBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class User
+/**
+ * Class User
+ * @package App\Ptrio\MessageBundle\Entity
+ *
+ * @ORM\Entity
+ */
+class User extends BaseUser
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank()
+     */
+    protected $username;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @Assert\NotBlank()
+     */
+    protected $apiKey;
 }
