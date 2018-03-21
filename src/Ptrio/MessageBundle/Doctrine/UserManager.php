39aa5e680354f00d817068063240799ba566c4d1
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-21 12:32:30 +0100

    Create UserManager concrete class.

--- src/Ptrio/MessageBundle/Doctrine/UserManager.php
+++ src/Ptrio/MessageBundle/Doctrine/UserManager.php
@@ -1,15 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 21/03/2018
- * Time: 12:26
- */
 
 namespace App\Ptrio\MessageBundle\Doctrine;
 
+use App\Ptrio\MessageBundle\Model\UserManager as BaseUserManager;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserManager
+class UserManager extends BaseUserManager
 {
+    /**
+     * @var ObjectManager
+     */
+    private $objectManager;
 
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private $repository;
+
+    /**
+     * @var string
+     */
+    private $class;
+
+    /**
+     * UserManager constructor.
+     * @param ObjectManager $objectManager
+     * @param string $class
+     */
+    public function __construct(
+        ObjectManager $objectManager,
+        string $class
+    )
+    {
+        $this->objectManager = $objectManager;
+        $this->repository = $objectManager->getRepository($class);
+
+        $metadata = $objectManager->getClassMetadata($class);
+        $this->class = $metadata->getName();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function updateUser(UserInterface $user)
+    {
+        $this->objectManager->persist($user);
+        $this->objectManager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function removeUser(UserInterface $user)
+    {
+        $this->objectManager->remove($user);
+        $this->objectManager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findUserBy(array $criteria): ?UserInterface
+    {
+        /** @var UserInterface|null $user */
+        $user = $this->repository->findOneBy($criteria);
+        return $user;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getClass(): string
+    {
+        return $this->class;
+    }
 }
