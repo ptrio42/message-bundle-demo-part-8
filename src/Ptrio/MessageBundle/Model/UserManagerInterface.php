5c5fac83859833d0f9770487502040420e52fcf7
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-21 12:24:01 +0100

    Create UserManagerInterface.

--- src/Ptrio/MessageBundle/Model/UserManagerInterface.php
+++ src/Ptrio/MessageBundle/Model/UserManagerInterface.php
@@ -1,15 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 21/03/2018
- * Time: 12:19
- */
 
 namespace App\Ptrio\MessageBundle\Model;
 
+use Symfony\Component\Security\Core\User\UserInterface;
 
 interface UserManagerInterface
 {
+    /**
+     * @return UserInterface
+     */
+    public function createUser();
 
+    /**
+     * @param UserInterface $user
+     */
+    public function updateUser(UserInterface $user);
+
+    /**
+     * @param UserInterface $user
+     */
+    public function removeUser(UserInterface $user);
+
+    /**
+     * @param array $criteria
+     * @return null|UserInterface
+     */
+    public function findUserBy(array $criteria): ?UserInterface;
+
+    /**
+     * @param string $username
+     * @return null|UserInterface
+     */
+    public function findUserByUsername(string $username): ?UserInterface;
+
+    /**
+     * @return string
+     */
+    public function getClass(): string;
 }
