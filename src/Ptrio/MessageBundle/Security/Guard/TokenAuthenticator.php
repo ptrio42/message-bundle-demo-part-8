249647275e8136377800c01266b5578915db3562
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-21 15:19:37 +0100

    Create TokenAuthenticator.

--- src/Ptrio/MessageBundle/Security/Guard/TokenAuthenticator.php
+++ src/Ptrio/MessageBundle/Security/Guard/TokenAuthenticator.php
@@ -1,12 +1,86 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 21/03/2018
- * Time: 13:39
- */
-class TokenAuthenticator
-{
+namespace App\Ptrio\MessageBundle\Security\Guard;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
+
+class TokenAuthenticator extends AbstractGuardAuthenticator
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getCredentials(Request $request)
+    {
+        return [
+            'token' => $request->headers->get('X-AUTH-TOKEN'),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $apiKey = $credentials['token'];
+
+        if (null === $apiKey) {
+            return null;
+        }
+
+        return $userProvider->loadUserByUsername($apiKey);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        return new Response($exception->getMessage(), Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new Response('Authentication is required to access this resource.', Response::HTTP_UNAUTHORIZED);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(Request $request)
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsRememberMe()
+    {
+        return false;
+    }
 }
