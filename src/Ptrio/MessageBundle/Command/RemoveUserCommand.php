f82936bb84b7fcb3ef532948089d04bebd0d3941
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-24 11:18:52 +0100

    Create AddUserCommand class.

---
+++ src/Ptrio/MessageBundle/Command/RemoveUserCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.czarnoleski
+ * Date: 21/03/2018
+ * Time: 16:10
+ */
+
+namespace App\Ptrio\MessageBundle\Command;
+
+
+class RemoveUserCommand
+{
+
+}
---
+++ src/Ptrio/MessageBundle/Model/UserInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.czarnoleski
+ * Date: 21/03/2018
+ * Time: 15:54
+ */
+
+namespace App\Ptrio\MessageBundle\Model;
+
+
+interface UserInterface
+{
+
+}
