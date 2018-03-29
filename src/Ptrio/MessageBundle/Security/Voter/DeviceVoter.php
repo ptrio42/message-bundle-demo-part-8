9710148c4dad26412338bc2e0998035027cac8b7
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-03-29 19:43:31 +0200

    Create DeviceVoter class.

--- src/Ptrio/MessageBundle/Security/Voter/DeviceVoter.php
+++ src/Ptrio/MessageBundle/Security/Voter/DeviceVoter.php
@@ -1,12 +1,36 @@
 <?php
 
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 28/03/2018
- * Time: 15:21
- */
-class DeviceVoter
-{
+namespace App\Ptrio\MessageBundle\Security\Voter;
 
+use App\Ptrio\MessageBundle\Model\DeviceInterface;
+use App\Ptrio\MessageBundle\Model\UserInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class DeviceVoter extends Voter
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function supports($attribute, $subject)
+    {
+        return $subject instanceof DeviceInterface;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function voteOnAttribute($attribute, $subject, TokenInterface $token)
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+        /** @var DeviceInterface $device */
+        $device = $subject;
+
+        return $device->getUser() === $user;
+    }
 }
