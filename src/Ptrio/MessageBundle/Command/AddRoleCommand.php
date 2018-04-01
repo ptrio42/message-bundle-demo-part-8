8d788b14f8cd173e646e55981ddb52c65f7e3af0
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-04-01 02:19:19 +0200

    Add default roles as constants in UserInterface.php

---
+++ src/Ptrio/MessageBundle/Command/AddRoleCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.czarnoleski
+ * Date: 29/03/2018
+ * Time: 20:40
+ */
+
+namespace App\Ptrio\MessageBundle\Command;
+
+
+class AddRoleCommand
+{
+
+}
---
+++ src/Ptrio/MessageBundle/Command/RemoveRoleCommand.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: piotr.czarnoleski
+ * Date: 29/03/2018
+ * Time: 21:00
+ */
+
+namespace App\Ptrio\MessageBundle\Command;
+
+
+class RemoveRoleCommand
+{
+
+}
