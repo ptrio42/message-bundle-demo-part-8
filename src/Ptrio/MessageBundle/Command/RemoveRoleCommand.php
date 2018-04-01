65a74ad4c7d1429104dd9fd9c01a5ba296930d58
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-04-01 02:21:57 +0200

    Create RemoveRoleCommand

--- src/Ptrio/MessageBundle/Command/RemoveRoleCommand.php
+++ src/Ptrio/MessageBundle/Command/RemoveRoleCommand.php
@@ -1,15 +1,64 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 29/03/2018
- * Time: 21:00
- */
 
 namespace App\Ptrio\MessageBundle\Command;
 
+use App\Ptrio\MessageBundle\Model\UserManagerInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
-class RemoveRoleCommand
+class RemoveRoleCommand extends Command
 {
+    /**
+     * @var string
+     */
+    public static $defaultName = 'ptrio:message:remove-role';
 
+    /**
+     * @var UserManagerInterface
+     */
+    private $userManager;
+
+    /**
+     * AddRoleCommand constructor.
+     * @param UserManagerInterface $userManager
+     */
+    public function __construct(
+        UserManagerInterface $userManager
+    )
+    {
+        $this->userManager = $userManager;
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this->setDefinition([
+            new InputArgument('role', InputArgument::REQUIRED),
+            new InputArgument('username', InputArgument::REQUIRED),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $role = $input->getArgument('role');
+        $username = $input->getArgument('username');
+
+        if ($user = $this->userManager->findUserByUsername($username)) {
+            $user->removeRole($role);
+            $this->userManager->updateUser($user);
+
+            $output->writeln($role.' role has been removed from user: '.$user->getUsername());
+        } else {
+            $output->writeln('The user with the given username cannot be found.');
+        }
+    }
 }
