f7570665e50598fab0be59d8afe52ba7074a57e8
piotr42 <piotr.czarnoleski@ampmedia.pl>
2018-04-07 13:45:52 +0200

    Add MessageRepository class

--- src/Ptrio/MessageBundle/Repository/MessageRepository.php
+++ src/Ptrio/MessageBundle/Repository/MessageRepository.php
@@ -1,15 +1,30 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: piotr.czarnoleski
- * Date: 01/04/2018
- * Time: 02:47
- */
 
 namespace App\Ptrio\MessageBundle\Repository;
 
+use App\Ptrio\MessageBundle\Model\DeviceInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 
-class MessageRepository
+class MessageRepository extends EntityRepository implements MessageRepositoryInterface
 {
+    public function __construct(EntityManagerInterface $em, string $class)
+    {
+        parent::__construct($em, $em->getClassMetadata($class));
+    }
 
+    public function findMessagesByDevice(DeviceInterface $device, string $sort = 'DESC', int $offset, int $limit): array
+    {
+        $qb = $this->getEntityManager()->createQueryBuilder();
+
+        $qb
+            ->select('m')
+            ->from($this->getEntityName(), 'm')
+            ->andWhere($qb->expr()->eq('m.device', $device->getId()))
+            ->orderBy('m.id', $sort)
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+        ;
+        return $qb->getQuery()->getResult();
+    }
 }
